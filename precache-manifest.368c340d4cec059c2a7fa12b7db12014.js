909e7a10c466dd17acf1220073067f3ecd55cfba
Zoé Velten <zoevelten@Zoes-MacBook-Air.local>
2020-08-14 07:10:12 +0200

    Updates

---
+++ precache-manifest.368c340d4cec059c2a7fa12b7db12014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389854fc062f5052d825ea609fa12211",
+    "url": "/bitcoin-final/index.html"
+  },
+  {
+    "revision": "ea9f7d37919ee5960909",
+    "url": "/bitcoin-final/static/css/main.8e08307d.chunk.css"
+  },
+  {
+    "revision": "8e35b294ebe00e7071af",
+    "url": "/bitcoin-final/static/js/2.540bafea.chunk.js"
+  },
+  {
+    "revision": "e2ebf032bac9ee9300e99b9004ef445f",
+    "url": "/bitcoin-final/static/js/2.540bafea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9f7d37919ee5960909",
+    "url": "/bitcoin-final/static/js/main.097f5499.chunk.js"
+  },
+  {
+    "revision": "d643820855d74c60c45a",
+    "url": "/bitcoin-final/static/js/runtime-main.9dc412d0.js"
+  }
+]);
